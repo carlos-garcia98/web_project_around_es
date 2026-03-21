5d9bb6ce173b8cd73076e9461a13619b1599c484
carlos-garcia98 <carlos0698@live.com>
2026-03-21 15:37:36 -0600

    Create functionality so the user can edit the name profile and description profile by creating funcions and event listeners

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # Tripleten web_project_around_es
+
 ## Project description
+
 This project (part of the Tripleten bootcamp) aims to bring a static website to life using JavaScript. The project is divided into sections; this Readme will be updated as each section is developed.
 
 ### Part 1:
+
 In the first part, I created a ‘script’ directory and an ‘index.js’ file, and linked them to the ‘index.html’ file. The main aim of this first part was to create an array of six objects, each of which represents one of the cards that are currently hard-coded. Each object has a ‘name’ key with the value of the name of a place in the US, and a ‘link’ key with the value of a link to an image of that place.
 
 ### Part 2:
 
+In the second part, I added functionality allowing the user to edit the profile name and description. I achieved this by selecting DOM elements in JavaScript and creating the following functions and events:
+
+- Open the modal containing the form for editing the profile name and description by clicking the ‘Edit Profile’ button.
+- Close the modal containing the form for editing the profile name and description by clicking the close button.
+- Capture the values entered into the text inputs to update the values of the elements that store the profile name and description
+- Capture the values entered into the text inputs to update the values of the elements storing the profile name and description when the form is submitted.
+
 ### Part 3:
 
 ## Technologies
-* JavaScript
+
+- JavaScript
--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,57 @@ const initialCards = [
 initialCards.forEach((card) => {
   console.log(card.name);
 });
+
+//Edit profile
+//Variables
+const editProfileBtn = document.querySelector(".profile__edit-button");
+const editProfileModal = document.querySelector("#edit-popup");
+const closeEditProfileModalBtn =
+  editProfileModal.querySelector(".popup__close");
+const editProfileForm = editProfileModal.querySelector("#edit-profile-form");
+const editProfileNameInput = editProfileForm.querySelector(
+  ".popup__input_type_name",
+);
+const editProfileDescriptionInput = editProfileForm.querySelector(
+  ".popup__input_type_description",
+);
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+//functions
+function opneModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+function fillProfileForm() {
+  editProfileNameInput.value = profileTitle.textContent;
+  editProfileDescriptionInput.value = profileDescription.textContent;
+}
+
+function handleOpenEditModal() {
+  fillProfileForm();
+  opneModal(editProfileModal);
+}
+
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
+
+  const nameInputValue = editProfileNameInput.value;
+  const descriptionValue = editProfileDescriptionInput.value;
+
+  profileTitle.textContent = nameInputValue;
+  profileDescription.textContent = descriptionValue;
+
+  closeModal(editProfileModal);
+}
+
+//Event Listeners
+editProfileBtn.addEventListener("click", handleOpenEditModal);
+closeEditProfileModalBtn.addEventListener("click", function () {
+  closeModal(editProfileModal);
+});
+editProfileForm.addEventListener("submit", handleProfileFormSubmit);
