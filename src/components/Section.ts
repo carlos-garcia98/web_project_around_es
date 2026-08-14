9b15a3ee3b3accbe30839e2344901654bdb2d0c8
carlos-garcia98 <carlos0698@live.com>
2026-08-14 14:06:07 -0600

    Crete Section class that will work to render HTML elements to the DOM.

---
+++ src/components/Section.ts
@@ -0,0 +1,25 @@
+type RendererFunction<T> = (item: T) => void;
+
+export class Section<T> {
+  private _items: T[];
+  private _renderer: RendererFunction<T>;
+  private _containerSelector: HTMLElement;
+
+  constructor(
+    { items, renderer }: { items: T[], renderer: RendererFunction<T>}, containerSelector: string
+  ) {
+    this._items = items;
+    this._renderer = renderer;
+    this._containerSelector = document.querySelector(containerSelector) as HTMLElement;
+  }
+
+  renderer() {
+    this._items.forEach(item => {
+      this._renderer(item);
+    });
+  }
+
+  addItem(element: HTMLElement) {
+    this._containerSelector.append(element);
+  }
+}
